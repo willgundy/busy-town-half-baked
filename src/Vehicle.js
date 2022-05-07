31544e473a3672562d04e92dbf5944c1f87e2ab9
Will Gundy <willgunderson503@gmail.com>
2022-05-06 18:27:00 -0700

    fixed the vehicle and vehicleList files

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,9 +1,16 @@
-export default function Vehicle() {
+export default function Vehicle({ vehicle }) {
   return <div className="vehicle">
     {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {/* if the vehicle props is a car, render a '🚗' emoji */}
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
+    { { 'car': <div>🚗</div>,
+      'truck': <div>🚚</div>,
+      'bus': <div>🚌</div>,
+      'motorcycle': <div>🏍️</div>,
+    } [vehicle]
+    }
+
   </div>;
 }
--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Vehicle from './Vehicle';
 
 export default function VehicleList({ vehicles }) {
   return (
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
+      {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
+      {vehicles.map((vehicle, i) => 
+        <Vehicle 
+          key={i}
+          vehicle={vehicle}
+        />
+      )}
     </div>
   );
 }
