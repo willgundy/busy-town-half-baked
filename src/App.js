b4e6a674654606cc6aac28a5e82d51c593ebe62a
Will Gundy <willgunderson503@gmail.com>
2022-05-06 18:02:48 -0700

    updated fight game to work

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   // lizardSize should be a number that starts out as 10
   // alienSize should be a number that starts out as 10
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
-  const [lightcolor, setLightColor] = useState('red');
+  const [lightColor, setLightColor] = useState('red');
   const [lizardSize, setLizardSize] = useState(10);
-  const [alienSize, setalienSize] = useState(10);
+  const [alienSize, setAlienSize] = useState(10);
   const [traffic, setTraffic] = useState(['car', 'truck']);
   
 
@@ -20,22 +20,22 @@ function App() {
       <div className="fight">
         <div className="monster">
           {/* the width of the alien should be ten times whatever the alien size is in state */}
-          <img src="alien.png" width={20} />
+          <img src="alien.png" width={10 * alienSize} />
           <div className='buttons'>
             {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh no! The alien is gobblin up all the electricity!</button>
+            <button onClick={() => {setAlienSize(alienSize + 1); }}>Oh no! The alien is gobblin up all the electricity!</button>
             {/* when you click this button, the lizard's size in state should go down by one */}
-            <button >Amazing! The alien zapped the lizard!</button>
+            <button onClick={() => {setLizardSize(lizardSize - 1); }}>Amazing! The alien zapped the lizard!</button>
           </div>
         </div>
         <div className="monster">
           {/* the width of the lizard should be ten times whatever the alien size is in state */}
-          <img src="lizard.png" width={20} />
+          <img src="lizard.png" width={10 * lizardSize} />
           <div className="buttons">
             {/* when you click this button, the lizard's size in state should go up by one */}
-            <button>Yegads! The lizard is ramping up to its final form!</button>
-            {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh my! The lizard chomped down on the alien!</button>
+            <button onClick={() => {setLizardSize(lizardSize + 1); }}>Yegads! The lizard is ramping up to its final form!</button>
+            {/* when you click this button, the alien's size in state should go down by one */}
+            <button onClick={() => {setAlienSize(alienSize - 1); }}>Oh my! The lizard chomped down on the alien!</button>
           </div>
         </div>
       </div>
